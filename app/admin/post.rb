7f6fd65927372eefa2449751c232129c1ae77d66
Diosan <diosan@diosan-VirtualBox.(none)>
2013-11-27 14:57:36 -0400

    Configure post index page in admin and add description to posts model

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -9,6 +9,7 @@ ActiveAdmin.register Post do
       end
       
       f.inputs "Content" do
+        f.input :description
         f.input :text, :input_html => { :class => "admin_tinymce", :rows => 20, :cols => 60 }
       end
       f.actions
@@ -16,16 +17,34 @@ ActiveAdmin.register Post do
     
     show do
       div do
+        post.description
+      end
+      div do
         image_tag(post.image_url(:main).to_s)
       end
       div do
         simple_format post.text
-      end
-      
+      end      
     end
     
-   index :as => :grid do |post|
-    link_to(image_tag(post.image_url(:thumb).to_s), admin_post_path(post))
-  end
+    index do
+      column "Title" do |post|
+        div do 
+          h2 do
+            link_to(post.title, admin_post_path(post))
+          end
+        end
+      end
+      column "Description" do |post|
+        p do
+          simple_format post.description
+        end
+      end
+      column "Image" do |post|
+        image_tag(post.image_url(:thumb).to_s)
+      end
+      column :created_at
+      column :updated_at
+    end
 
 end
