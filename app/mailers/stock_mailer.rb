5879b3ceec6c6409efd8a91419a0495b0572a975
Diosan <diosan@diosan-VirtualBox.(none)>
2013-12-17 19:10:22 -0400

    Stock Low mailer main build ... need to check count_on_hand first now....

--- app/mailers/stock_mailer.rb
+++ app/mailers/stock_mailer.rb
@@ -1,7 +1,10 @@
 class StockMailer < ActionMailer::Base
   def stock_low_notification(stock_item)
-    Spree:RoleUser.where(role_id: 1).each do | role_user |
-      mail(:to => role_user.email, :subject => "#{Spree::Config[:site_name]} Low Stock Notification", :from => "dion.santana@gmail.com")
+    @product = stock_item.variant.product
+    @product_name = @product.name
+    @product_link = main_app.root_url + "shop/admin/products/" + stock_item.variant.product.permalink + "/stock"
+    Spree::Role.find(1).users.each do | admin_user |
+      mail(:to => admin_user.email, :subject => Spree::Config[:site_name] + ": Stock Low Notification for '" + stock_item.variant.product.name + "'", :from => Spree::Config[:mails_from])
     end
   end
 end
